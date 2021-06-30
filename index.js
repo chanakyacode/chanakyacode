8d7ba0ebd98e3aa9795222d23c64b3fb4d8b9fa6
mohit <mohittherising@gmail.com>
2021-06-30 19:22:41 +0530

    30 june second

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const btn = document.querySelector(".menu-toggle")
 const nav = document.querySelector("nav")
 
 btn.addEventListener("click", () => { nav.classList.toggle("active"); })
+
+var moon = document.getElementById("moon");
+
+moon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        moon.src = "images/sun.png";
+    } else {
+        moon.src = "images/moon.png";
+    }
+}
